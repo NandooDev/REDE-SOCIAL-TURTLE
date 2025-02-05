9195cee72896d912a09e6d92cf18518002f0cb68
NandooDev <familiaforte0876@gmail.com>
2025-02-05 16:22:24 -0300

    feat: add cors

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { config } from "dotenv";
 import { routes } from "./routes/routes";
 import path from "path";
+import cors from "cors";
 
 config();
 
@@ -9,6 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use("/files", express.static(path.resolve("uploads")));
+app.use(cors());
 
 const port = process.env.PORT || 3000;
 
