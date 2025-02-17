d9d11f3d23112aa3e99144d572a3f96c9b865870
NandooDev <familiaforte0876@gmail.com>
2025-02-17 14:59:00 -0300

    update: update in response from newAcessTokenController

--- src/controllers/auth/newAcessToken/newAcessTokenController.ts
+++ src/controllers/auth/newAcessToken/newAcessTokenController.ts
@@ -3,9 +3,7 @@ import { IAuthController, INewAcessToken } from "./protocols";
 
 export class NewAcessTokenController implements IAuthController {
   constructor(private readonly newAcessToken: INewAcessToken) {}
-  async handle(
-    httpRequest: IHttpRequest<any>
-  ): Promise<IHttpResponse<string>> {
+  async handle(httpRequest: IHttpRequest<any>): Promise<IHttpResponse<string>> {
     try {
       const { refreshToken } = httpRequest.body;
 
@@ -16,7 +14,15 @@ export class NewAcessTokenController implements IAuthController {
         };
       }
 
-      const newAcessToken = await this.newAcessToken.newAcessToken(refreshToken);
+      const newAcessToken =
+        await this.newAcessToken.newAcessToken(refreshToken);
+
+      if (newAcessToken === "User not found") {
+        return {
+          statusCode: 401,
+          body: "User not found",
+        };
+      }
 
       return {
         statusCode: 200,
